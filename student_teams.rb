f90968b8005d0c310cb0da19a8ebe6f64cc500f8
Postel <matthewpostel@gmail.com>
2015-07-30 14:11:34 -0400

    program complete

--- student_teams.rb
+++ student_teams.rb
@@ -1,5 +1,15 @@
 
-students =["Megan","Eamon","Keith","Ryan","Brian","Cecelia","Brant","Postel"]
+students =[]
+
+puts "Enter Attendance!"
+print "Type 'done' when finished!"
+
+input = gets.chomp
+
+until input == 'done'
+	students.push(input)
+	input = gets.chomp
+end
 
 students.shuffle!
 
@@ -7,7 +17,19 @@ count = 0
 
 until count == students.length 
 #while count < students.length 
-	puts "#{students[count]} & #{students[count+1]}"
-
-	count += 2
+   if students.length%2 == 1
+   	if students.length == 1
+   		puts "#{students[count]}"
+   		count += 1
+	elsif count < students.length - 3 
+		puts "#{students[count]} & #{students[count+1]}"
+		count += 2
+	else
+		puts "#{students[count]}, #{students[count+1]} & #{students[count+2]}"
+		count += 3
+	end
+   else
+   		puts "#{students[count]} & #{students[count+1]}"
+		count += 2
+   end
 end
